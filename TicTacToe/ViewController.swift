9e0db8be2338e766c5e7301eb05abc5e41f04297
Abdul <ferdnorbert@gmail.com>
2019-02-04 19:46:22 -0500

    Basic Biolerplate

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var turn = 1
+    var board = [0,0,0,0,0,0,0,0,0]
+    
+    var winning_conditions = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    
+    var gameOver = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func handleClick(_ sender: AnyObject) {
+        
+        if(board[sender.tag] == 0 && !gameOver){
+        if(turn == 1){
+            
+            sender.setImage(UIImage(named : "cross.png"), for: [])
+            
+            board[sender.tag] = 1
+            turn = 2
+            
+        }
+        else if(turn == 2){
+            
+            sender.setImage(UIImage(named : "nought.png"), for: [])
+            board[sender.tag] = 2;
+            turn = 1
+            
+        }
+        }
+        
+        for iterator in winning_conditions{
+            
+        }
+        print(board)
+    }
+    
 }
 
